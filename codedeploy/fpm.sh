2665cf8ef5113f65c306f61021c642bb75ae6431
adam goucher <adam@goucher.ca>
2022-03-09 21:05:12 -0500

    Update fpm.sh

--- codedeploy/fpm.sh
+++ codedeploy/fpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-if service --status-all | grep -Fq 'php7.1-fpm'; then    
-  sudo service php7.1-fpm reload
+if service --status-all | grep -Fq 'php7.3-fpm'; then    
+  sudo service php7.3-fpm reload
 else
   sudo service php7.0-fpm reload
 fi
